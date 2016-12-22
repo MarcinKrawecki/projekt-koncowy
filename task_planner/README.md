f5413ec65b529424621eff2dd06a1f14f520fbb7
Marcin Krawecki <marcin.krawecki@gmail.com>
2016-12-22 10:41:36 +0100

    fos_user i tabela

---
+++ task_planner/README.md
@@ -0,0 +1,4 @@
+task_planner
+============
+
+A Symfony project created on December 22, 2016, 9:58 am.
---
+++ task_planner/src/TaskPlannerBundle/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcin
+ * Date: 22.12.16
+ * Time: 10:09
+ */
+
+namespace TaskPlannerBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+/*
+    /**
+     * @ORM\OneToMany(targetEntity="Task", mappedBy="User")
+     */
+   // private $task;
+   // /**
+    // * @ORM\OneToMany(targetEntity="Category", mappedBy="User")
+    // */
+   // private $category;*/
+
+}
